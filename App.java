e04fe20b9152bb54982f760d47facc9486dec1f6
Isha0914 <shahid.ismail0914@gmail.com>
2023-01-26 11:40:59 +0100

    add first instances, add constructor

--- App.java
+++ App.java
@@ -1,3 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class App{
-    
+private JFrame frame;
+private JLabel title;
+private JButton addButton;
+private JButton betweenButton;
+
+public App() {
+
+}
+public static void main(String[] args) {
+    App app = new App();
+}
 }
